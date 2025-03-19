d13877f244d8094499b38660f9cef226f9f01239
yassine snaiki <yassnaiki@gmail.com>
2025-03-19 11:59:48 +0000

    ticket model

---
+++ app/Models/Ticket.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Ticket extends Model
+{
+    /** @use HasFactory<\Database\Factories\TicketFactory> */
+    use HasFactory;
+    protected $fillable = [
+        'customer_id', 'agent_id', 'title', 'description', 'status', 'priority', 'created_at', 'updated_at'
+    ];
+
+    // Relationships
+    public function customer()
+    {
+        return $this->belongsTo(Customer::class);
+    }
+
+    public function agent()
+    {
+        return $this->belongsTo(Agent::class);
+    }
+
+}
