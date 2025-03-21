ef7cf7aa48aa5b17a367d581c8223b4b258cc451
yassine snaiki <yassnaiki@gmail.com>
2025-03-21 12:06:46 +0000

    updating user migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
-            $table->enum('role', ['customer', 'agent']);
+            $table->enum('role', ['customer', 'agent','admin'])->default('customer');
             $table->timestamps();
         });
 
